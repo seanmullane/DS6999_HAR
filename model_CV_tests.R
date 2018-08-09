ee369a9104f4acbaba2dd89a88e5b1c367a67400
seanmullane <mullane.s.p@gmail.com>
2018-08-09 14:16:29 -0400

    Fixed t-tests
    Fixed data ordering for t-tests and filled out R code to include all model runs and print result tables

--- model_CV_tests.R
+++ model_CV_tests.R
@@ -1,7 +1,8 @@
 
 # This script is for training models on accelerometer sensor data for HAR
 
-setwd("C:/Users/spm9r/eclipse-workspace-spm9r/DS6999_HAR/")
+#setwd("C:/Users/spm9r/eclipse-workspace-spm9r/DS6999_HAR/")
+setwd("~/DSI/classes/DS6999/DS6999_HAR/")
 
 # --------------------------------------------------------------------------------------------------------
 # Load libraries
@@ -25,44 +26,82 @@ reslist_logitboost <- modelResultsList
 load("svm/svmLinear_model_results.RData")
 reslist_svmlinear <- modelResultsList
 
+l.allmodels <- list(reslist_glmnet, reslist_knn, reslist_logitboost, reslist_svmlinear)
+names(l.allmodels) <- c("GLMnet", "KNN", "LogitBoost", "SVMLinear")
+
 # --------------------------------------------------------------------------------------------------------
 # Data processing
 # --------------------------------------------------------------------------------------------------------
 
-rindv <- c(5, 10, 25, 50)
-cindv <- c(1, 2, 3, 5, 10)
 
-m <- matrix(data = NA, nrow = 4, ncol = 5)
-
-# Create matrices for results
-
-m.acc <- m
-m.sd <- m
-l.samples <- list(20)
-for (i in 1:length(reslist_glmnet)) {
-  #print(reslist_glmnet[[i]]) 
-  rind <- which(rindv == reslist_glmnet[[i]]$data_frequency)
-  cind <- which(cindv == reslist_glmnet[[i]]$data_window)
-  print(rind, cind)
-  samples <- reslist_glmnet[[i]]$model_resample$Accuracy
-  l.samples[[i]] <- samples
-  m.acc[rind, cind] <- round(mean(samples), 4)
-  m.sd[rind, cind] <- round(sd(samples), 4)
+l.alloutputs <- list()
+for (j in 1:4) {
+  
+  reslist <- l.allmodels[[j]]
+  
+  # Create data frame to pre-sort results
+  l_r_a <- list(20)
+  for (i in 1:20) { 
+    l_r_a[[i]] <- as.list(reslist[[i]]$model_resample$Accuracy)
+  }
+  d.res <- data.frame(freq = as.integer(unlist(lapply(reslist, "[[", "data_frequency"))),
+                      window = as.integer(unlist(lapply(reslist, "[[", "data_window"))))
+  m.acc.sample <- matrix(unlist(l_r_a), nrow = 20, ncol = 10, byrow = T)
+  d.accsample <- as.data.frame(m.acc.sample)
+  d.res <- cbind(d.res, m.acc.sample)
+  d.res <- d.res %>% arrange(window, freq)
+  rm(l_r_a, m.acc.sample)
+  
+  d.res2 <- d.res %>%
+    gather(samp_elem, model_acc, -freq, -window) %>%
+    group_by(freq, window) %>%
+    summarise(mean_acc = mean(model_acc),
+              sd_acc = sd(model_acc)) %>%
+    ungroup %>%
+    arrange(window, freq)
+  
+  # Create matrix to contain results
+  rindv <- c(5, 10, 25, 50)
+  cindv <- c(1, 2, 3, 5, 10)
+  
+  m <- matrix(data = NA, nrow = 4, ncol = 5)
+  colnames(m) <- paste0(cindv, "s")
+  rownames(m) <- paste0(rindv, "Hz")
+  
+  # Populate matrices for results
+  m.acc <- m
+  m.sd <- m
+  for (i in 1:20) {
+    m.acc[i] <- mean(unlist(d.res[i,3:12]))
+    m.sd[i] <- sd(unlist(d.res[i,3:12]))
+  }
+  
+  print(m.acc)
+  print(m.sd)
+  
+  # Best window & freq, get list index of best mean and all other list indices
+  best_m_inds <- arrayInd(which.max(m.acc), dim(m.acc), dimnames(m.acc), T); best_m_inds
+  best_ind <- which.max(d.res2$mean_acc)
+  other_inds <- which(best_ind != 1:20)
+  
+  # Get p-values from hypothesis tests
+  m.pval <- m
+  for (i in other_inds) {
+    # One-sided hypothesis test (Welch Two Sample t-test)
+    # H_0: difference in mean accuracy between samples is 0
+    # H_A: mean accuracy in sample 1 is greater than mean accuracy in sample 2
+    tt <- t.test(unlist(d.res[best_ind,3:12]), unlist(d.res[i,3:12]), alternative = "greater", mu = 0)
+    m.pval[[i]] <- round(tt$p.value, digits = 4)
+  }
+  
+  print(m.pval)
+  
+  l.alloutputs[[j]] <- list(m.acc, m.sd, m.pval)
 }
-colnames(m.acc) <- paste0(cindv, "s")
-colnames(m.sd) <- paste0(cindv, "s")
-rownames(m.acc) <- paste0(rindv, "Hz")
-rownames(m.sd) <- paste0(rindv, "Hz")
 
-print(m.acc)
-print(m.sd)
+for (j in 1:4) {
+  print(names(l.allmodels)[[j]])
+  print(l.alloutputs[[j]][[3]])
+}
 
-# Best window & freq
-best_inds <- arrayInd(which.max(m.acc), dim(m.acc), dimnames(m.acc), T); best_inds
-max(m.acc)
-
-# One-sided hypothesis test (Welch Two Sample t-test)
-# H_0: difference in mean accuracy between samples is 0
-# H_A: mean accuracy in sample 1 is greater than mean accuracy in sample 2
-tt <- t.test(l.samples[[which.max(m.acc)]], l.samples[[1]], alternative = "greater", mu = 0); tt
-
+# Need to apply bonferroni correction for multiple comparisons
